3787a855c2dc316cfcfe7f2a951af2d491ea50fa
RebeccaFJ01 <rebeccaj.fyhn@gmail.com>
2026-09-24 10:53:14 +0200

    class summary

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // Husk fra dag 1: skriv "use strict" herunder
 
 
@@ -6,19 +7,31 @@ const getToggleBtn = document.getElementById("toggleBtn");
 
 // Skriv selv: hent de tre andre elementer på samme måde, ved hjælp af deres id.
 // Variablerne skal hedde getToggleText, getToggleIcon og getMoreInfo
-
+const getToggleText = document.getElementById("toggleText");
+const getToggleIcon = document.getElementById("toggleIcon");
+const getMoreInfo = document.getElementById("moreInfo");
 
 // Eksempel: vi lytter efter klik på knappen og kører en anonym function
-getToggleBtn.addEventListener("click", function() {
+getToggleBtn.addEventListener("click", function() { 
 
     // Skriv if/else-strukturen selv herinde.
     //
     // Nyt i dag: getMoreInfo.hidden viser/skjuler et element med det samme (true = skjult, false = synligt).
     //
+    if (getMoreInfo.hidden === true) {
+        getMoreInfo.hidden = false;
+        getToggleText.textContent = `vis mindre`;
+        getToggleIcon.style.transform = `rotate(180deg)`; 
+    }
     // Hvis getMoreInfo.hidden er true (dvs. indholdet er skjult lige nu), så:
     //   - sæt getMoreInfo.hidden til false
     //   - sæt getToggleText.textContent til `Vis mindre`
     //   - sæt getToggleIcon.style.transform til `rotate(180deg)`
+    else {
+        getMoreInfo.hidden === true 
+        getToggleText.textContent = `Vis mere`;
+        getToggleIcon.style.transform = `rotate(0deg)`;
+    }
     // Ellers:
     //   - sæt getMoreInfo.hidden til true
     //   - sæt getToggleText.textContent til `Vis mere`
